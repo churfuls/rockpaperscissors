b9098501c0ab57c325ebc92118d3b1305aaa78db
churfuls <CHERLY809@GMAIL.COM>
2019-11-06 22:56:03 +0000

    I did it!

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
-
-
-// DOCUMENT READY FUNCTION BELOW
-
+$("#shoot").click(function() {
+  var inpput = $("#input").val();
+$("#userChoice").append(input);
+});
