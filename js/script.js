efdc79688efbae4b4736ab14ea9c42fbbfdebbc9
churfuls <CHERLY809@GMAIL.COM>
2019-11-13 22:52:55 +0000

    I don't know why paper doesn't work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 $("#shoot").click(function() {
   var inpput = $("#input").val();
 $("#userChoice").append(input);
+var go = Math.random();
+$("#computerChoice").text(go)
+if (go > .70){
+$("#computerChoice").text("Rock");
+}
+else if (go > .40){
+$("#computerChoice").text("Scissors");
+}
+else {
+    ("#computerChoice").text("Paper");
+}
 });
