57237882b4167c0a906d53e6c4d60067e9f7d833
churfuls <CHERLY809@GMAIL.COM>
2019-11-20 23:02:42 +0000

    editted again

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ $("#userChoice").text(input);
 var go = Math.random();
 
 if (go > .70){
-$("#computerChoice").text("Rock");
+$("#computerChoice").text("rock");
 }
 else if (go > .40){
-$("#computerChoice").text("Scissors");
+$("#computerChoice").text("scissors");
 }
 else {
     console.log("i hate you!");
-    $("#computerChoice").text ("Paper");
+    $("#computerChoice").text ("paper");
 }
 var computerChoice = $("#computerChoice").val();
 
-if (input === "rock" && Computerchoice === "rock" ){
+if (input === "rock" && computerChoice === "rock" ){
     Message = ("You both suck")
-    $("#result").text ("message")
+    $("#result").text("message")
 }
 });
